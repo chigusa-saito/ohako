e64704b98daf3707d44e5d67db2039a2cd58faff
齋藤千種 <chitanemaru@gmail.com>
2025-01-03 23:18:43 +0900

    https://www.bring-flower.com/blog/how-to-add-custom-fields/がうまくいかない

--- functions.php
+++ functions.php
@@ -10,64 +10,77 @@ add_theme_support( 'post-thumbnails' );
 
 
 
-add_action('init', 'create_post_type');
-function create_post_type()
-{
-    //投稿時に使用できる投稿用のパーツを指定
-    $supports = array(
-        'title', //タイトルフォーム
-        'editor', //エディター(内容の編集)
-        'thumbnail', //アイキャッチ画像
-        'author', //投稿者
-        'excerpt', //抜粋
-        'revisions', //リビジョンを保存
+//カスタムフィールドのメタボックス
+function add_custom_fields(){
+  $post_id = '';
+  if(isset($_GET['post']) || isset($_POST['post_ID'])) {
+    $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+  }
+  if ($post_id == get_page_id('top')){
+   add_meta_box(
+      'custom_field_01', //セクションのID
+      '店舗住所', //セクションのタイトル
+      'insert_custom_fields', //フォーム部分を指定する関数
+      'page', //投稿タイプの場合は「post」、カスタム投稿タイプの場合は「スラッグ名」、固定ページの場合は「page」
+      'normal', //セクションの表示場所
+      'high'  //優先度
     );
-    register_post_type(
-        'sample', // 投稿タイプ名の定義
-        [
-        'labels' => [
-            'name' => 'サンプル投稿', // 管理画面上で表示する投稿タイプ名
-        ],
-        'public'        => true,  // カスタム投稿タイプの表示(trueにする)
-        'has_archive'   => true, // カスタム投稿一覧(true:表示/false:非表示)
-        'menu_position' => 5,     // 管理画面上での表示位置
-        'show_in_rest'  => false,  // true:「Gutenberg」/ false:「ClassicEditor」
-        'supports' => $supports
-        ]
-    );
+  }
 }
-
-
-
-
-add_action('admin_menu', 'create_custom_fields');
-function create_custom_fields()
+add_action('admin_menu', 'add_custom_fields');
+//ページIDを取得する関数の定義
+function get_page_id($post_name)
 {
-    add_meta_box(
-        'sample_setting', //編集画面セクションID
-        'サンプルカスタムフィールド', //編集画面セクションのタイトル
-        'insert_custom_fields', //編集画面セクションにHTML出力する関数
-        'page', //投稿タイプ名
-        'normal' //編集画面セクションが表示される部分
-    );
+  global $wpdb;
+  $id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = '".$post_name."'");
+  return esc_html($id);
 }
-
-function insert_custom_fields()
-{
-    global $post;
-    $sample = get_post_meta($post->ID, 'sample', true); ?>
-
-<form method="post" action="admin.php?page=site_settings">
-    <label for="sample">サンプルフォーム：</label>
-    <input id="sample" type="text" name="sample" value="<?php echo $sample ?>">
-</form>
-
-<?php
+//カスタムフィールドの入力エリア
+function insert_shop_fields() {
+  global $post;
+  //nounceフィールドの追加
+  wp_nonce_field('custom_field_save_meta_box_data', 'custom_field_meta_box_nonce');
+echo '
+<table class="custom-fields">
+  <tr class="border-top border-bottom">
+     <th scope="row">
+       <label for="shop_a">店舗A</label>
+     </th>
+     <td> 
+       <input type="text" id="shop_a" name="shop_a" value="'.get_post_meta($post_id, 'shop_a', true).'" />
+     </td>
+   </tr>
+   <tr class="border-bottom">
+      <th scope="row">
+        <label for="shop_b">店舗B</label>
+      </th>
+      <td> 
+        <input type="text" id="shop_b" name="shop_b" value="'.get_post_meta($post_id, 'shop_b', true).'" />
+      </td>
+   </tr>
+   <tr class="border-bottom">
+   <th scope="row">
+     <label for="shop_c">店舗C</label>
+   </th>
+   <td> 
+     <input type="text" id="shop_c" name="shop_c" value="'.get_post_meta($post_id, 'shop_c', true).'" />
+   </td>
+</tr>
+</table>
+';
+}
+//カスタムフィールドの値を保存
+function save_custom_fields( $post_id ){
+  //nonceがセットされているか確認
+  if (!isset($_POST['custom_field_meta_box_nonce'])) {
+   return;
+  }
+ //nounceが正しいか検証
+   if (!wp_verify_nonce($_POST['custom_field_meta_box_nonce'], 'custom_field_save_meta_box_data')) {
+    return;
+   }
+  $key = 'shop_a'; if(!empty($_POST[$key])){update_post_meta($post_id,'$key',$_POST[$key]);}else{delete_post_meta($post_id,'$key');}
+  $key = 'shop_b'; if(!empty($_POST[$key])){update_post_meta($post_id,'$key',$_POST[$key]);}else{delete_post_meta($post_id,'$key');}
+  $key = 'shop_c'; if(!empty($_POST[$key])){update_post_meta($post_id,'$key',$_POST[$key]);}else{delete_post_meta($post_id,'$key');}
 }
 add_action('save_post', 'save_custom_fields');
-function save_custom_fields($post_id)
-{
-    if (isset($_POST['sample'])) {
-        update_post_meta($post_id, 'sample', $_POST['sample']);
-    }
-}
