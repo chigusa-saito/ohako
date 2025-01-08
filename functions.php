62b27bd12e4115182885a4cfdba9547768c62154
齋藤千種 <chitanemaru@gmail.com>
2025-01-08 09:22:33 +0900

    contactform7でショートコードでプライバシーポリシーacfを表示する

--- functions.php
+++ functions.php
@@ -5,7 +5,14 @@ add_theme_support( 'post-thumbnails' );
 
 
 // カスタムフィールド
+// プライバシーポリシーで使用する
 
-
-
+function get_privacypolicy() {
+  $page_obj = get_page_by_path( 'contact' );
+  $page_id = $page_obj->ID;
+  $privacypolicy = get_field('privacypolicy',$page_id);
+  return $privacypolicy;
+}
+add_shortcode('show_privacypolicy', 'get_privacypolicy');
+wpcf7_add_shortcode('show_privacypolicy','get_privacypolicy'); 
 
